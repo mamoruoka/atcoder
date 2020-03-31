2d037846252e61bf436163364dda9fa61480d140
das <oka.mamoru@gmail.com>
2020-03-31 14:41:14 +0900

    [AC] abc051 abc051_c

---
+++ atcoder.jp/abc051/abc051_c/Main.py
@@ -0,0 +1,7 @@
+sx,sy,tx,ty=map(int,input().split())
+
+ans='U'*(ty-sy)+'R'*(tx-sx)+'D'*(ty-sy)+'L'*(tx-sx)
+ans+='L'+'U'*(ty-sy+1)+'R'*(tx-sx+1)+'D'
+ans+='R'+'D'*(ty-sy+1)+'L'*(tx-sx+1)+'U'
+
+print(ans)
